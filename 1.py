eaa72a704dc02737c335b651f9d1d2ab451dd76e
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-02 12:50:42 +0330

    first commit

---
+++ 1.py
@@ -0,0 +1,22 @@
+# print('hi')
+# print('how are you?')
+# print(1)
+# print(1+2)
+# print(1+2*3)
+# print(1+3/3)
+# print(1.5)
+# print()
+# print('---------')
+# print('****************')
+
+# + - * /
+# print(1+2)
+# print(1-2)
+# print(1*2)
+# print(1/2)
+
+# تقدم ی اولویت : یعنی کدام باید اول انجام شود
+print(5 + 2 * 3)
+print(5 + 2 / 2)
+print(5 + 2 - 2)
+print(5 * 2 / 2)
