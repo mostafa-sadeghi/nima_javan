47e288ac2400c117a887924e84cb19879cf5ef40
mostafa-sadeghi <msai.sadeghi@gmail.com>
2022-12-09 12:44:52 +0330

    18azar

--- 2.py
+++ 2.py
@@ -1,2 +1,50 @@
-print(2 + 3 * 5)
-print((2+3)*5)
+# print(2 + 3 * 5)
+# print((2+3)*5)
+
+# x = 2
+# print(x)
+# x = 3
+# print(x)
+
+# x = 0
+# print(type(x))
+
+x = ''
+print(type(x))
+x = ""
+print(type(x))
+
+y = 'a'
+y = 'abcdefg'
+print(type(y))
+# نامگذاری متغیرها
+
+'''
+ 1. نام مناسب
+ 2. امکان استفاده از کاراکترهای خاص به غیر از _ وجود ندارد
+ 3. از - نمی توانیم برای نام گذاری متغیرها استفاده نمائیم
+ 4.از فاصله هم توانیم استفاده کنیم
+ 5. از عدد در ابتدای نام نمی توانیم استفاده کنیم
+'''
+
+
+# _x = 2  # no problem
+# x-  = 2  # error
+# 2x = 2 # error
+
+# valid variable names:
+# هر تر کیبی از کاراکتر ها ، اعداد و _ به شرطی که اولین حرف عدد نباشد
+# x1 = 2
+# name1 = 'ali'
+# name_1 = 'ali'
+# _name_1 = 'ali'
+
+# snake case & camelCase
+
+# snake_case
+# student_name = 'ali'
+
+# camel case
+# studentName = 'ali'
+# big camel case
+StudentName = 'ali'
---
+++ 3.py
@@ -0,0 +1,65 @@
+# message = 'hello every body'
+# print(message)
+# message = "hello every body"
+# print(message)
+
+# message = "he said: "go to school""
+# message = 'he said: "go to school"'
+# message = '''he said: "go to school"'''
+# message = "he said: \"go to school\""
+# print(message)
+# message = '''hello every body
+# this application is used for some thing
+# '''
+# message = """hello every body
+# this application is used for some thing
+# """
+# print(message)
+
+# message = 'he said don'nt do this'
+# message = "he said don'nt do this"
+# message = 'he said don\'nt do this'
+# message = '''he said don'nt do this'''
+# message = """he said don'nt do this"""
+# print(message)
+
+name = "nima"
+age = 13
+
+# message = "%s is %s years old" % (name, age)
+# fstring or formatted string
+# message = f"{name} is {age} years old"
+# print(message)
+# print(f"{name} is {age} years old")
+# print(name, "is", age, "years old")
+
+# + - * /
+# concat یا چسباندن رشته ها
+# s1 = "a"
+# s2 = "b"
+# s = s1 + s2
+# s = s1 + 1  # type error
+# print(s)
+
+# message = name + " " + "is" + " " + str(age) + " " + "years old"
+# print(message)
+
+
+# Exercise 1:
+'''
+برنامه ای بنوس که چهار عدد را در چهار متغیر ذخیره نمایدو سپس:
+متغیر پنجمی تعریف کنید که حاوی عبارت زیر باشد:
+عدد دوم را در عدد سوم ضرب نماید و سپس عدد اول را از حاصلضرب بدست سآمده تفریق نماید
+درنهایت حاصل عبارت بدست آمده با عدد چهارم جمع شده و در متغیر پنجم ذخیره شود
+در انتها مقدار متغیر پنجم نمایش داده شود.
+
+'''
+
+# exercise 2:
+'''
+سه متغیر تعریف کن که داخل هر کدام نام یکی از اعضای خانواده قرا داشته  باشد
+در نهایت یک متغیر جدید تعریف کن که حاوی عبارت زیر باشد
+ali and tina and nikan are family
+و در نهایت عبارت در ترمینال پرینت شود
+
+'''
