8f6c66888a48c8e4e85d677d4aca235e19423d78
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-04 17:00:22 +0330

    sadeghi

---
+++ past/review1.py
@@ -0,0 +1,160 @@
+# print('hello')
+# print('2 + 3 * 2')
+# print(2 + 3 * 2)
+
+# print('hi.\nwelcome to our python class.')
+# print('id\tname')
+# print('01\tarman')
+# print('02\tarmin')
+
+
+# print('hi.\rhello.')
+
+# print('hello.\rhi.')
+
+
+# print('hello.\rwelcome to our python class.\rwelcome to our pygame')
+# score = 0
+# print("my score is:", score)
+
+# score = score + 1
+# print("my score is:", score)
+
+# score += 1
+# print("my score is:", score)
+
+# score = score - 1
+# print("my score is:", score)
+
+# score -= 1
+# print("my score is:", score)
+
+# x = 5
+
+# x + 1 == 5
+
+# print(3/2)  # تقسیم اعشاری
+# print(3//2)  # تقسیم صحیح
+# % باقیمانده
+
+# print(4 % 2)
+# print(5 % 2)
+# number = int(input('enter an int number:> '))
+# if number % 2 == 0:
+#     print(f'{number} is even.')
+
+# else:
+#     print(f'{number} is odd.')
+
+# یک تابع بساز که عدد را به عنوان پارامتر بگیرد و
+# در صورت زوج بودن آن عدد،
+# even
+# در صورت فرد بودن عدد
+# odd
+# را برگرداند
+# در نهایت مقدار برگردانده شده توسط تابه را پرینت کن.
+
+# def calculateAverage(name, sc1, sc2, sc3):
+#     average = (sc1 + sc2 + sc3) / 3
+#     return f'{name} your average is {average:.1f}'
+
+
+# name = input('enter your name: ')
+# score1 = float(input('enter score1: '))
+# score2 = float(input('enter score2: '))
+# score3 = float(input('enter score3: '))
+
+# ave = calculateAverage(name, score1, score2, score3)
+# print(ave)
+
+
+# print("The answer to 10+10 is," 10+10)
+# a. The answer to 10+10 is 10+10
+# b. The answer to 10+10 is 20
+# c. The answer to 10+10 is10+10
+# d. The answer to 10+10 is20
+# e. The answer to 10+10 is,10+10
+# f. The answer to 10+10 is,20
+# g. Nothing, the code is invalid.
+
+
+# print("Hello")
+# a. Hello
+# b. "Hello"
+# c. Nothing, there is an error in the code.
+# d. Nothing, the code is commented out.
+
+
+# x = 10
+# x + 1
+# print(x)
+# a. 10
+# b. 11
+# c. "x"
+# d. x
+# e. Nothing
+
+
+# x = 10 + 6 / 2
+# x = x + 1
+# print(x)
+
+# a. 8
+# b. 9
+# c. 13
+# d. 14
+# e. Nothing
+
+
+# print("Have a "great" day!")
+# a. "Have a "great" day!"
+# b. Have a "great" day!
+# c. Have a great day!
+# d. Nothing, the double quotes make for a syntax error.
+
+# print("Save in c:\new folder")
+
+# a. Save in c:\new folder
+# b. Save in c:
+# ew folder
+# c. Save in c:ew folder
+# d. Nothing, the escape code makes for a syntax error.
+
+
+# print("Save in c:\\new folder")
+
+# از کدام یک از گزینه های زیر می توان به عنوان نام متغیر استفاده نمود:
+# apple
+# Apple
+# APPLE
+# Apple2
+# 1Apple
+# account number
+# account_number
+# account.number
+# accountNumber
+# account#
+# pi
+# PI
+# fred
+# Fred
+# GreatBigVariable
+# greatBigVariable
+# great_big_variable
+# great.big.variable
+# 2x
+# x2x
+# total%
+#left
+
+
+# خروجی کد زیر چیست؟ 
+# print(a)
+# a = 45
+
+# خروجی کد زیر چیست؟
+
+x = 4
+y = 5
+a = ((x) * (y))
+print(a)
