e9a6854ecc4091e9aba0c52050498140fe3ad994
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-10 13:02:43 +0330

    sadeghi

---
+++ past/review2.py
@@ -0,0 +1,212 @@
+# a = 4
+# b = 4
+# Basic comparisons
+# if a < b:
+#  print("a is less than b")
+# if a > b:
+#  print("a is greater than b")
+# print("Done")
+
+
+# if a <= b:
+#     print("a is less than or equal to b")
+# if a >= b:
+#     print("a is greater than or equal to b")
+
+# a = 4
+# b = 4
+# if a == b:
+#  print("a is equal to b")
+# # Not equal
+# if a != b:
+#  print("a and b are not equal")
+
+# if not a==b:
+#  print("a and b are not equal")
+
+# name = input("enter students's name:> ")
+# age = int(input("enter students's age:> "))
+
+# if age < 8:
+#     print(f'{name} is kid.')
+# if age >= 8 and age < 13:
+#     print(f'{name} is junior.')
+# # if 8 <= age < 13:
+# #     print(f'{name} is junior.')
+# if age >= 13 and age < 18:
+#     print(f'{name} is teenager.')
+# else:
+#     print(f'{name} is adult.')
+
+
+# if True and True and True:
+#     print('*******')
+# if True and False:
+#     print('*******')
+# if False and True:
+#     print('*******')
+# if False and False:
+#     print('*******')
+
+
+# if True or True or True:
+#     print('*******')
+# if True or False:
+#     print('*******')
+# if False or True:
+#     print('*******')
+# if False or False:
+#     print('*******')
+
+
+# a = True
+# if a:
+#     print(True)
+# if True:
+#     print(True)
+
+
+# if 1 < 2:
+#     print("ok")
+
+# if True:
+#     print("ok")
+# if 1 > 2:
+#     print("ok")
+
+
+# a = True
+# if not a:
+#     print('*')
+
+
+# a = False
+# if not a:
+#     print('*')
+
+
+# a = 3
+# b = 3
+# c = a == b
+# print(c)
+
+
+# if 1:
+#     print("1")
+
+# if 0:
+#     print("0")
+# if -1:
+#     print("-1")
+# print(bool(1))
+# print(bool(0))
+# print(bool(-1))
+
+# if '':
+#     print("something")
+# if 'a':
+#     print("siklflksfds")
+
+
+# USER = 'root'
+# PASSWD = 'root'
+
+# user_name = input('enter the user name:> ')
+# password = input('enter the password:> ')
+# if user_name and password:
+#     if user_name == USER and password == PASSWD:
+#         print('you can login now')
+#     else:
+#         print('user name or password is not correct...')
+
+
+# else:
+#     print('you must enter your user name and/or password')
+
+
+# entry = input('enter a name or "exit" to exit from program:> ')
+# if entry == 'exit':
+#     print('good bye')
+#     exit()
+# print("balalall")
+# if True:
+#     print("ok")
+
+
+# USER = ['root', 'admin']
+# PASSWD = 'root'
+
+# user_name = input('enter the user name:> ')
+# password = input('enter the password:> ')
+# if user_name and password:
+#     if (user_name == 'root' or user_name == 'admin') and password == PASSWD:
+#         print('you can login now')
+#     else:
+#         print('user name or password is not correct...')
+
+
+# else:
+#     print('you must enter your user name and/or password')
+
+
+#################################################################
+
+# user_name = input('enter your user name:> ')
+# if user_name.lower() == 'nima':
+#     print("you are valid user.")
+# else:
+#     print("you are not valid user.")
+
+
+# کدام شرط می گوید که
+# a < b
+
+
+# a. if a less than b:
+# b. if a < b
+# c. if a > b
+# d. if a < b:
+# e. if (a < b)
+# f. if a >= b
+# g. if a <= b:
+
+
+# برابری
+
+# a. if a equals b:
+# b. if a = b
+# c. if a = b:
+# d. if a == b:
+# e. if a == b
+# f. if a === b
+# g. if a === b:
+
+
+# کوچکتر یا مساوی
+
+# a. if a < or = b:
+# b. if a <= b:
+# c. if a < = b:
+# d. if a >= b:
+# e. if a =< b:
+# f. if a < b or == b:
+# g. if a <== b:
+
+# a
+# هم از
+# b
+# کوچکتر است
+# هم از
+# c
+
+# a. if a < b and < c:
+# b. if a < b & c:
+# c. if a < b and a < c:
+# d. if a < b and c:
+
+
+a = 2
+b = 3
+c = 4
+if a < b & c:
+    print("ok")
