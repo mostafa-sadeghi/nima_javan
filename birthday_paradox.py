22a3c6864a2afd6f14e5e2322ca375322203d4bd
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-07 12:56:14 +0330

    sadeghi

---
+++ birthday_paradox.py
@@ -0,0 +1,47 @@
+import datetime
+import random
+
+
+def get_birthdays(number_of_peoples):
+    birthdays = []
+
+    start_date = datetime.date(2013, 1, 1)
+    for i in range(number_of_peoples):
+        random_numbers_of_days = datetime.timedelta(random.randrange(0, 365))
+        birthdays.append(start_date + random_numbers_of_days)
+    return birthdays
+
+
+def getMatch(birthdays):
+    for i in range(len(birthdays)):  # ['1','2','3',.....'23']
+        for j in range(i+1, len(birthdays)):
+            if birthdays[i] == birthdays[j]:
+                return True
+    return False
+
+
+print('''Birthday paradox.
+The shows us that in a group of n people, probablity of matching birthdays is surprisingly increasing when n become bigger.
+in 23 numbers of people The probability is greater than 50%,
+and in 70 numbers of people this probability is 99.9%
+''')
+
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+while True:
+    print("How many birthdays shall I generate? (Max = 100)")
+    resp = input('> ')
+    if resp.isdecimal() and (0 < int(resp) <= 100):
+        numDays = int(resp)
+        break
+print()
+print("Here are", numDays, 'birthdays')
+birthdays = get_birthdays(numDays)
+
+for birthday in birthdays:
+    print(birthday.day, MONTHS[birthday.month])
+print()
+
+match = getMatch(birthdays)
+
+# Display the results
--- s15.py
+++ s15.py
@@ -1,3 +1,4 @@
+# import matplotlib.pyplot as plt
 numbers = [1, 2, 3, 4, 5, 6]
 # print(numbers[0])
 
@@ -30,31 +31,51 @@ numbers = [[1, 2], [3, 4], [5, 6]]
 # print(result)
 
 
-numbers = [ [1,2,6,3] , [3,4,12,44] , [5,6,67,89] ]
-res = 0
-for i in range(len(numbers)):  #     i = 1       j = 2       numbers[0] = [1,2,6,3]         res = 19
-    for j in range(len(numbers[i])):
-        res += numbers[i][j]
+# numbers = [ [1,2,6,3] , [3,4,12,44] , [5,6,67,89] ]
+# res = 0
+# for i in range(len(numbers)):  #     i = 1       j = 2       numbers[0] = [1,2,6,3]         res = 19
+#     for j in range(len(numbers[i])):
+#         res += numbers[i][j]
 
-print(res)
+# print(res)
 
 
 # numbers = []
 # تعدادی عدد از ورودی بگیر و به لیست بالا اضافه کن
 # با کمک حلقه فور و وایل مجموع اعداد رو حساب
-numbers = []
-for i in range(3):
-    number = int(input('enter a number'))
-    numbers.append(number)
+# numbers = []
+# for i in range(3):
+#     number = int(input('enter a number'))
+#     numbers.append(number)
 
-print(numbers)
+# print(numbers)
 
-res = 0
-for i in range(len(numbers)):
-    res += numbers[i]
+# res = 0
+# for i in range(len(numbers)):
+#     res += numbers[i]
 
-print(res)
+# print(res)
 
 
-numbers = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h'], ['i', 'j', 'k', 'l']]
+# numbers = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h'], ['i', 'j', 'k', 'l']]
 # با کمک دو حلقه تک تک کاراکترها را پرینت
+
+
+image = [
+    [255, 202, 0],
+    [120, 22, 33],
+    [100, 0, 255],
+]
+# plt.imshow(image, cmap='gray')
+# plt.show()
+# input()
+COLORS = ["white", "gray","black"]
+for row in range(3): # row = 0 - 2              2
+    for col in range(3): # col = 0 - 2          2
+        if image[row][col] == 0:
+            image[row][col] = "black"
+        elif image[row][col] == 255:
+            image[row][col] = "white"
+        else:
+            image[row][col] = "gray"
+print(image)
